b53ec4d99ea6cd4590da65153fbc6c7eea0711f6
CCtuhulu <wajdimhamdi999@gmail.com>
2024-03-25 17:49:34 +0100

    modified:   telnet_config.py

--- telnet_config.py
+++ telnet_config.py
@@ -1,23 +1,23 @@
-from adressage_automatique import *
-import ipaddress
-
-
-# Configure Loopback Interface(已完成)
-def config_loopback(loopback_address, protocol):
+#Configure Loopback Interface
+def config_loopback(ip_loopback, protocol):
     config = []
+    netmask0 = ip_loopback.split("/")[1]
+    netmask1 = str(256 - 2**(32-int(netmask0)))
+    netmask = "255.255.255.x".replace("x",netmask1)
 
     config.append("enable")
     config.append("conf t")
     config.append("interface Loopback0")
-    config.append(f"ip address {loopback_address}")
+    config.append(f"ip address {ip_loopback}")
     config.append("no shutdown")
 
     if protocol == "OSPF":
-        config.append(" ip ospf 2002 area 0")
+        config.append("ip ospf 666 area 0")
 
     config.append("end")
 
     return config
+
 
 
 # Configure each interface(已完成)
