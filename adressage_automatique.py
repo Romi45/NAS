f599ebcb2dca930b6a9ca2baf12c740c319aec53
CCtuhulu <wajdimhamdi999@gmail.com>
2024-03-25 16:47:22 +0100

    allocation automatique marche bien approuvé

---
+++ adressage_automatique.py
@@ -0,0 +1,106 @@
+import json
+
+class Router:
+    def __init__(self, name, type, interfaces):
+        self.name = name
+        self.type = type
+        self.loopback_address = ""
+        self.interfaces = interfaces
+        
+
+    def __str__(self):
+        return f"Router(Name: {self.name}, Type: {self.type}, Loopback: {self.loopback_address}, Interfaces: {self.interfaces})"
+
+class AS:
+    def __init__(self, type, ip_range, protocol, routers):
+        self.type = type
+        self.ip_range = ip_range
+        self.protocol = protocol
+        self.routers = [Router(router['name'], router['type'], router['interfaces']) for router in routers]
+
+    def __str__(self):
+        router_str = '\n  '.join(str(router) for router in self.routers)
+        return f"AS(type: {self.type}, IP Range: {self.ip_range}, Protocol: {self.protocol}, Routers:\n  {router_str})"
+    
+
+def generate_ip(iteration,iteration_client, router, AS, Autre_AS):
+
+    #Loop sur toutes les interfaces et check si on leur a deja assigner une addresse ip    
+    for interface in router.interfaces:
+
+        if "ip_address" not in interface:
+            neighbour_name = interface["neighbor"]
+
+            not_same_as = True
+
+
+            for r in AS.routers:
+                if r.name == neighbour_name:
+                    ip_address = (AS.ip_range).replace('x',str(iteration*4 + 1))
+                    interface["ip_address"] =  ip_address
+                    not_same_as = False
+
+
+                    #Cherche l'interface correspondante sur l'autre routeur de la connexion
+                    for rr in AS.routers:
+                        if rr.name == neighbour_name:
+
+                            for interf in rr.interfaces:
+                                if interf["neighbor_interface"] == interface["name"]:
+                                    mirror_ip_address = (AS.ip_range).replace('x',str(iteration*4 + 2))
+                                    interf["ip_address"] =  mirror_ip_address
+
+
+                    break
+
+
+            #Assignation des addresses ip pour les routeurs de bordure
+            if not_same_as:
+                ip_address = (Autre_AS.ip_range).replace('x','1').replace('y',str(iteration_client))
+                interface["ip_address"] =  ip_address
+
+
+                for rr in Autre_AS.routers:
+                        if rr.name == neighbour_name:
+
+                            for interf in rr.interfaces:
+                                if interf["neighbor_interface"] == interface["name"]:
+                                    mirror_ip_address = (Autre_AS.ip_range).replace('x','2').replace('y',str(iteration_client))
+                                    interf["ip_address"] =  mirror_ip_address
+                                    iteration_client += 1
+
+    
+    return iteration_client
+
+
+def generate_loopback(loopback_iteration,router):
+
+    #Loopback assigner selon l'ordre de declaration des routeurs dans le fichier intent
+    loopback_address = "x.x.x.x/32".replace("x",str(loopback_iteration))
+    router.loopback_address = loopback_address
+
+
+with open('new intent.json', 'r') as file:
+    data = json.load(file)
+
+all_as = [AS(as_info['type'], as_info['ip_range'], as_info['protocol'], as_info['routers']) 
+          for as_info in data['AS']]
+
+
+iteration = 0
+iteration_client = 0
+for as_index,as_content in enumerate(all_as):
+    for router in as_content.routers:
+        autre_as_index = 0 if as_index==1 else 1
+        iteration_client = generate_ip(iteration, iteration_client, router, all_as[as_index], all_as[autre_as_index])
+        iteration += 1
+
+iteration_loopback = 1
+for as_content in all_as:
+    for router in as_content.routers:
+        generate_loopback(iteration_loopback,router)
+        iteration_loopback += 1
+
+
+print(all_as[0])
+print(all_as[1])
